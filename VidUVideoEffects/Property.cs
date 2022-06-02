b968230db3f88906084c2350ee4f2093ad4ff681
Doslan Zheksheev <doslan130400@gmail.com>
2022-06-02 19:48:27 +0600

    VidU Effects Commit

---
+++ VidUVideoEffects/Property.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Collections;
+
+namespace VidUVideoEffects
+{
+    internal class Property
+    {
+        private bool IsMuzU;
+        private Dictionary<float, float> Values;
+        private string TransitionType;
+        private double Value;
+        private double MinValue;
+        private double MaxValue;
+
+        public Property(string name, IPropertySet configuration)
+        {
+            Debug.WriteLine("♥♥♥ " + configuration.Select(it => "[" + it.Key + ":" + it.Value + "]\r").Aggregate(string.Empty, (s, v) => s + v));
+            IsMuzU = (bool)configuration[nameof(IsMuzU)];
+            if(!IsMuzU) Value = (double)configuration[name+"_"+nameof(Value)];
+            else
+            {
+                Values = (Dictionary<float, float>)configuration[nameof(Values)];
+                Debug.WriteLine("♥♥♥ " + Values.Select(it => "[" + it.Key + ":" + it.Value + "]\r").Aggregate(string.Empty, (s, v) => s + v));
+                TransitionType = (string)configuration[name + "_" + nameof(TransitionType)];
+                MinValue = (double)configuration[name + "_" + nameof(MinValue)];
+                MaxValue = (double)configuration[name + "_" + nameof(MaxValue)];
+            }
+        }
+
+        private Random random = new Random();
+        private double trnTime = 0.1;
+        private double trnValue = 0.2;
+        internal float Get(double time)
+        {
+            if (!IsMuzU) return Convert.ToSingle(Value);
+            else
+            {
+                float lastTime = new List<float>(Values.Keys).FindLast(it => it <= time);
+                double v = Values[lastTime];
+                double tv = 0;
+                if (time - lastTime < trnTime) tv = trnValue * (trnTime - time + lastTime) / trnTime;
+                switch (TransitionType)
+                {
+                    case "None": break;
+                    case "Growth": v -= tv; break;
+                    case "Decay": v += tv; break;
+                    case "Shake": v += tv*(random.NextDouble()*2-1); break;
+                }
+                if (v < 0) v = 0;
+                if (v > 1) v = 1;
+                return Convert.ToSingle(v * (MaxValue - MinValue) + MinValue);
+            }
+        }
+    }
+}
---
+++ VidUVideoEffects/Vignette.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Foundation.Collections;
+using Windows.Graphics.DirectX.Direct3D11;
+using Windows.Media.Effects;
+using Windows.Media.MediaProperties;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using System.Numerics;
+using System.Diagnostics;
+
+namespace VidUVideoEffects
+{
+    public sealed class Vignette : IBasicVideoEffect
+    {
+        public void ProcessFrame(ProcessVideoFrameContext context)
+        {
+            using (CanvasBitmap inputBitmap = CanvasBitmap.CreateFromDirect3D11Surface(_canvasDevice, context.InputFrame.Direct3DSurface))
+            using (CanvasRenderTarget renderTarget = CanvasRenderTarget.CreateFromDirect3D11Surface(_canvasDevice, context.OutputFrame.Direct3DSurface))
+            using (CanvasDrawingSession ds = renderTarget.CreateDrawingSession())
+            {
+                TimeSpan time = context.InputFrame.RelativeTime.HasValue ? context.InputFrame.RelativeTime.Value : new TimeSpan();
+                if (time.TotalSeconds < Start || time.TotalSeconds > End)
+                {
+                    ds.DrawImage(inputBitmap);
+                    return;
+                }
+
+                // START HERE
+                var vignette = new VignetteEffect()
+                {
+                    Source = inputBitmap,
+                    Amount = 0.9f,
+                    Curve = Amount.Get(time.TotalSeconds)
+                };
+                ds.DrawImage(vignette);
+            }
+        }
+
+        private Property Amount;
+
+        float Start;
+        float End;
+        public void SetProperties(IPropertySet configuration)
+        {
+            Start = Convert.ToSingle(configuration["Start"]);
+            End = Convert.ToSingle(configuration["End"]);
+            //START HERE
+            Amount = new Property(nameof(Amount), configuration);
+        }
+
+        private CanvasDevice _canvasDevice;
+
+        public void SetEncodingProperties(VideoEncodingProperties encodingProperties, IDirect3DDevice device)
+        {
+            //_currentEncodingProperties = encodingProperties;
+            _canvasDevice = CanvasDevice.CreateFromDirect3D11Device(device);
+            CanvasDevice.DebugLevel = CanvasDebugLevel.Error;
+        }
+
+        public bool IsReadOnly { get { return false; } }
+        public MediaMemoryTypes SupportedMemoryTypes { get { return MediaMemoryTypes.Gpu; } }
+        public bool TimeIndependent { get { return false; } }
+
+        public IReadOnlyList<VideoEncodingProperties> SupportedEncodingProperties
+        {
+            get
+            {
+                return new List<VideoEncodingProperties>()
+                {
+                    // NOTE: Specifying width and height is only necessary due to bug in media pipeline when
+                    // effect is being used with Media Capture. 
+                    // This can be changed to "0, 0" in a future release of FBL_IMPRESSIVE. 
+                    VideoEncodingProperties.CreateUncompressed(MediaEncodingSubtypes.Argb32, 800, 600)
+                };
+            }
+        }
+
+        public void Close(MediaEffectClosedReason reason)
+        {
+            // Clean up devices
+            if (_canvasDevice != null)
+                _canvasDevice.Dispose();
+        }
+
+        public void DiscardQueuedFrames()
+        {
+            // No cached frames to discard
+        }
+    }
+}
